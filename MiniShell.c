7ce71776d3e134940ba67b4a6ba8015c29452eb9
Andy Leo <andyleo97@gmail.com>
2018-10-08 23:57:40 -0500

    got everything working

--- MiniShell.c
+++ MiniShell.c
@@ -1,15 +1,19 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "unistd.h"
+#include <sched.h>
+#include <unistd.h>
+#include <pwd.h>
 
 void readLine(char *line);
 void loop();
 void createArgs(char *str, char **argVect);
 void execute(char **args);
-
+int myClone(void *);
+int mapCloneArgs(char **args);
 
 int main() {
 
@@ -19,17 +23,42 @@ int main() {
 }
 
 void loop(){
-  char *str = malloc(sizeof(char*) * 64);
-  char **args = malloc(sizeof(char**) * 64);
-
+  char *str = malloc(sizeof(char*) * 1024);
+  char **args = malloc(sizeof(char**) * 1024);
+  void **child_stack;
   while(1){
     printf("%s","mini-shell> ");
     readLine(str);
     createArgs(str, args);
     if (strcmp(args[0], "exit") == 0) {
+      free(str);
+      free(args);      
       break;
     }
+    else if (strcmp(args[0], "clone") == 0){
+	int cloneArg = mapCloneArgs(args);
+	if (cloneArg == 0) {
+	     continue;
+	}
+	int stat;
+	child_stack = malloc(16384); 
+	pid_t retId = clone(myClone, child_stack, mapCloneArgs(args) | SIGCHLD, NULL);	
+	pid_t id = waitpid(retId, &stat, 0);
+	continue;
+    }
+    else if(strcmp(args[0], "cd") == 0){
+        char *newDir = args[1];
+	if(args[1] == NULL || strcmp(args[1], "~") == 0){
+	   newDir = getenv("HOME");
+       }	    
+       int error = chdir(newDir);
+       if(error != 0){
+          printf("Change Directory Failed\n");
+       } 
+    }
+    else {
     execute(args);
+    }
   }
 
 }
@@ -72,3 +101,32 @@ void execute(char **args){
           ;
      }
 }
+
+int myClone(void *args){
+loop();
+return 0;
+}
+
+int mapCloneArgs(char **args){
+   if(args[1] != NULL){
+	
+      if(strcmp(args[1], "ns")== 0){
+        return 0x00020000;
+      }
+      if(strcmp(args[1], "ipc")== 0){  
+        return 0x08000000;
+      }
+      if(strcmp(args[1], "user")== 0){  
+        return 0x10000000;
+      }
+      if(strcmp(args[1], "net")== 0){  
+        return 0x40000000;
+      }
+      if(strcmp(args[1], "uts")== 0){  
+        return 0x04000000;
+      }
+   } 
+   printf("Usage: clone ns | net | ipc | user | uts\n");
+   return 0;
+   
+}	
